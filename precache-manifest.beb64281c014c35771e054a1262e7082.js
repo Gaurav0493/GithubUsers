4d35b22fcc4a2ac9a16e6645ec96326b94a1e74d
Gaurav Kumar <gauravkr93.gk@gmail.com>
2019-06-10 12:17:50 +0530

    Updates

---
+++ precache-manifest.beb64281c014c35771e054a1262e7082.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e352244ddf7362d3be1a256e6cab39ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "79fd5053e86df662915c",
+    "url": "./static/css/main.460909af.chunk.css"
+  },
+  {
+    "revision": "e0c74ab6984e62690804",
+    "url": "./static/js/2.4a0b15a7.chunk.js"
+  },
+  {
+    "revision": "79fd5053e86df662915c",
+    "url": "./static/js/main.531b6782.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
